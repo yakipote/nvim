f693c5a4104051f83dfda28bae361bbc7e577652
kikuchi-fuminori1 <kikuchi-fuminori1@dmm.com>
2025-01-29 17:09:04 +0900

    add scrollbar

---
+++ lua/plugins/scrollbar.lua
@@ -0,0 +1,8 @@
+return {
+  "petertriho/nvim-scrollbar",
+  dependencies = { "lewis6991/gitsigns.nvim" },
+  config = function()
+    require("scrollbar").setup()
+    require("scrollbar.handlers.gitsigns").setup()
+  end,
+}
