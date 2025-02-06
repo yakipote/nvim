af1f086c87f373489ed606156a706b15211e5274
kikuchi-fuminori1 <kikuchi-fuminori1@dmm.com>
2025-02-06 13:05:01 +0900

    add keymap settings

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -4,3 +4,17 @@
 -- Insertモードで 'jj' または 'kk' を押すとESCキーにマップ
 vim.api.nvim_set_keymap("i", "jj", "<ESC>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("i", "kk", "<ESC>", { noremap = true, silent = true })
+
+local map = vim.keymap.set
+
+local uname = vim.uv.os_uname()
+print(uname.sysname)
+
+-- macos用の設定
+if uname.sysname == "Darwin" then
+  -- Resize window using <ctrl> arrow keys
+  map("n", "<M-Up>", "<cmd>resize +2<cr>", { desc = "Increase Window Height" })
+  map("n", "<M-Down>", "<cmd>resize -2<cr>", { desc = "Decrease Window Height" })
+  map("n", "<M-Left>", "<cmd>vertical resize -2<cr>", { desc = "Decrease Window Width" })
+  map("n", "<M-Right>", "<cmd>vertical resize +2<cr>", { desc = "Increase Window Width" })
+end
