24157b3f7f01131caf44bae47f2fcfddfb605935
yakipote <yakipote@redimpulz.com>
2025-03-25 01:58:32 +0900

    nerv

--- lua/plugins/nerv.lua
+++ lua/plugins/nerv.lua
@@ -1,5 +1,16 @@
 return {
-  -- dir = "~/workspace/nerv.nvim",
-  -- lazy = false,
-  -- priority = 1000,
+  {
+    "yakipote/nerv-theme.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {},
+  },
+  -- nvim-notify
+  {
+    "rcarriga/nvim-notify",
+    lazy = false,
+    config = function()
+      -- NERV-themeが自動的に設定するので、ここでは何もする必要はありません
+    end,
+  },
 }
